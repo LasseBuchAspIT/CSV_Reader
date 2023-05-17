2707a59da3ac72d599c9c5b9e91b209ddeeda0de
Lasse Emil Buch <lbu@ipnetcom.dk>
2023-05-17 10:06:34 +0200

    Made major changes to correct error caused by not being able to read a stream twice (fit both previous funtion into one and had it return both a bool and list instead)

--- Lib/Reader.cs
+++ Lib/Reader.cs
@@ -5,42 +5,16 @@ namespace Lib
 {
     public class Reader<T> where T : class
     {
-        public List<T> ConvertStreamToObjects(Stream stream)
+        public (List<T> list, bool delete) ConvertStreamToObjects(Stream stream)
         {
+            bool delete = false;
             List<T> list = new List<T>();
             string line;
 
             //create instance of creator
             Creator<T> creator = new();
 
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                //Converts to Objects
-                while((line = reader.ReadLine()) != null)
-                {
-                    try
-                    {
-                        //split line into seperate values
-                        string[] args = line.Split(";");
 
-                        //pass valuesi into creator to convert to object and add to list
-                        list.Add(creator.CreateT(args.ToList()));
-                        Console.WriteLine("Object added to list Succesfully");
-                    }
-                    catch 
-                    {
-                        Console.WriteLine("Line formatted incorrectly, skipping and moving on...");
-                    }
-                }
-            }
-
-            return list;
-        }
-
-        public bool GetBoolAndCutStreamToSize(Stream stream)
-        {
-            string line;
-            bool delete = false;
             using (StreamReader reader = new StreamReader(stream))
             {
 
@@ -58,8 +32,56 @@ namespace Lib
                         break;
                     }
                 }
+
+                //Converts to Objects
+                while ((line = reader.ReadLine()) != null)
+                {
+                    try
+                    {
+                        //split line into seperate values
+                        string[] args = line.Split(";");
+
+                        //pass valuesi into creator to convert to object and add to list
+                        list.Add(creator.CreateT(args.ToList()));
+                        Console.WriteLine("Object added to list Succesfully");
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Line formatted incorrectly, skipping and moving on...");
+                    }
+                }
+                
             }
-            return delete;
+            
+            return (list, delete);
+        }
+
+        public (bool asd, Stream str) GetBoolAndCutStreamToSize(Stream stream)
+        {
+            string line;
+            Stream ReturnStr = null;
+            bool delete = false;
+            using (StreamReader reader = new StreamReader(stream))
+            {
+
+                //read only first part of stream which contains headers and the deleteExisting bool
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    if (line == "true")
+                    {
+                        delete = true;
+                    }
+                    if (line == "Content-Type: text/csv")
+                    {
+                        //consume next line to avoid trying to create object from empty line
+                        reader.ReadLine();
+                        break;
+                    }
+                }
+                stream.CopyTo(ReturnStr);
+            }
+            return (delete, ReturnStr);
         }
     }
 }
