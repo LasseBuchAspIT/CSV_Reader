be9bccd633b3f48e79d19290d59b3b032ece9b7e
Lasse Emil Buch <lbu@ipnetcom.dk>
2023-05-17 10:07:19 +0200

    Added Adder class which should remove need for manually adding reader list to DB

---
+++ Lib/Adder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Adder<T> where T : class
+    {
+        DbSet<T> dbSet;
+        Reader<T> reader;
+
+        public Adder(DbSet<T> dbSet)
+        {
+            this.dbSet = dbSet;
+            reader = new Reader<T>();
+        }
+
+        private void AddListToDb(List<T> list)
+        {
+            foreach (T item in list) 
+            {
+                dbSet.Add(item);
+            }
+        }
+
+        public void AddStreamToDb(Stream stream)
+        {
+            foreach(T item in reader.ConvertStreamToObjects(stream).Item1)
+            {
+                dbSet.Add(item);
+            }
+        }
+
+    }
+}
