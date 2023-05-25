febed7843090400bf16fb76f4762b69b33fb8bbc
Lasse Emil Buch <lassebuch@live.dk>
2023-05-25 11:56:56 +0200

    Fixed bug where GetAll would not show updated database

--- CSV_Service/Service.cs
+++ CSV_Service/Service.cs
@@ -2,6 +2,7 @@
 using GenHTTP.Api.Protocol;
 using CSV_Reader.DAL;
 using Lib;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CSV_Reader
 {
@@ -29,15 +30,30 @@ namespace CSV_Reader
 
             adder.AddStreamToDb(input);
 
-            
-
+            //recreate context to get changes
+            context = new(SettingsReader.GetConnectionString("Settings.txt"));
             return new ValueTask<Task>();
         }
 
         [ResourceMethod("GetAll")]
         public List<Account> GetAll()
         {
-            return context.Accounts.ToList();
+            //quick makeshift solution, need to fix
+            List<Account> list = context.Accounts.ToList();
+            if (list[0].Fsa.IsNullOrEmpty())
+            {
+                list[0].Fsa = "";
+            }
+            if (list[0].Vip.IsNullOrEmpty())
+            {
+                list[0].Vip = "";
+            }
+            if (list[0].UserId == null)
+            {
+                list[0].UserId = 0;
+            }
+            return list;
+
         }
 
         [ResourceMethod("GetById")]
